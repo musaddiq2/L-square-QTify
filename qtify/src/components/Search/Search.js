62645000202df8d59bf593b57af229dd8a7edb62
musaddiq2 <uzumakibaruto093@gmail.com>
2023-11-23 18:16:27 +0530

    Module 1

--- qtify/src/components/Search/Search.js
+++ qtify/src/components/Search/Search.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Style from "./SearchBar.module.css";
 import { ReactComponent as SearchIcon } from "../../assets/Search-icon.svg";
-function Search(placeholder) {
+function Search({ placeholder }) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form className={Style.wrapper}>
-      <input className={Style.search}  placeholder="Search a album of your choice"/>
-      <button className={Style.searchButton}>
-        <SearchIcon />
+    <form className={Style.wrapper} onSubmit={onSubmit}>
+      <input className={Style.search} placeholder={placeholder} required />
+      <button className={Style.searchButton} type="submit">
+        {<SearchIcon />} {" "}
       </button>
     </form>
   );
